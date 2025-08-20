7f2f6b3c8a3ebfe5bba5af68189220d7da87263b
Arnav261 <arnavasher007@gmail.com>
2025-08-20 19:15:47 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 app.include_router(liftover.router, prefix="/liftover", tags=["liftover"])
 app.include_router(compare_annotations.router, prefix="/compare-annotations", tags=["compare"])
 app.include_router(upload_annotation.router, prefix="/upload-annotation", tags=["uploads"])
+
+@app.get("/")
+def root():
+    return {"message": "API is up!"}
