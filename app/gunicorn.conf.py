7b5184a1647732fe1aedce11fcc07299294f597a
Arnav261 <arnavasher007@gmail.com>
2025-08-27 11:57:51 +0530

    commit changes

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -1,12 +1,10 @@
 import multiprocessing
 
-# Point Gunicorn to your FastAPI app in main.py
 wsgi_app = "app.main:app"
-
 
 workers = int(multiprocessing.cpu_count() * 0.5) or 1
 worker_class = "uvicorn.workers.UvicornWorker"
-bind = "0.0.0.0:10000"   # Render expects dynamic port, defaults to 10000
+bind = "0.0.0.0:10000"
 timeout = 180
 graceful_timeout = 30
 keepalive = 5
